ecb385e1abc971f4653cc5dbb5331b32e1502781
ArabWare <80012174+arab-ware@users.noreply.github.com>
2022-10-23 21:10:54 +0300

    Add files via upload

---
+++ EveryThingUtils/src/main/java/com/android/prime/arab/ware/everythingutils/listeners/BitmapTasks.java
@@ -0,0 +1,7 @@
+package com.android.prime.arab.ware.everythingutils.listeners;
+
+public interface BitmapTasks {
+    public void done();
+    public void loading();
+    public void error(String error);
+}
