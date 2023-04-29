b47b0fb4aff27f9fa32877a6eb5c10cd1dd26009
ArabWare <80012174+arab-ware@users.noreply.github.com>
2023-04-29 09:34:49 +0300

    Add files via upload

---
+++ EveryThingUtils/src/main/java/chatgpt/sqlite/database/arabware/DatabaseToJson.java
@@ -0,0 +1,98 @@
+package chatgpt.sqlite.database.arabware;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaeToJson {
+
+    public interface ProgressListener {
+        void onProgressUpdate(int progress, int max);
+        void onConversionComplete(File outputFile);
+        void onConversionError(Exception e);
+    }
+
+    private final Context mContext;
+    private final ProgressListener mListener;
+
+    public DatabaseToJson(Context context, ProgressListener listener) {
+        mContext = context;
+        mListener = listener;
+    }
+
+    public void convertDatabaseToJson(File dbFile, File jsonFile) {
+        new Thread(() -> {
+            SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
+
+            Cursor cursor = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+            int count = cursor.getCount();
+
+            Gson gson = new GsonBuilder().create();
+
+            try (FileWriter writer = new FileWriter(jsonFile)) {
+                writer.write("[");
+                while (cursor.moveToNext()) {
+                    String tableName = cursor.getString(0);
+                    Cursor tableCursor = db.rawQuery("SELECT * FROM " + tableName, null);
+
+                    List<ContentValues> tableRows = new ArrayList<>();
+                    while (tableCursor.moveToNext()) {
+                        tableRows.add(cursorToJSON(tableCursor));
+                    }
+
+                    writer.write("{\"table_name\":\"" + tableName + "\",\"table_data\":");
+                    gson.toJson(tableRows, writer);
+                    writer.write("},");
+
+                    tableCursor.close();
+
+                    mListener.onProgressUpdate(tableRows.size(), count);
+                }
+                writer.write("]");
+
+                mListener.onConversionComplete(jsonFile);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                mListener.onConversionError(e);
+            } finally {
+                cursor.close();
+                db.close();
+            }
+        }).start();
+    }
+
+    private ContentValues cursorToJSON(Cursor cursor) {
+        ContentValues contentValues = new ContentValues();
+        String[] columns = cursor.getColumnNames();
+        for (String column : columns) {
+            int columnIndex = cursor.getColumnIndex(column);
+            switch (cursor.getType(columnIndex)) {
+                case Cursor.FIELD_TYPE_BLOB:
+                    contentValues.put(column, cursor.getBlob(columnIndex));
+                    break;
+                case Cursor.FIELD_TYPE_FLOAT:
+                    contentValues.put(column, cursor.getFloat(columnIndex));
+                    break;
+                case Cursor.FIELD_TYPE_INTEGER:
+                    contentValues.put(column, cursor.getInt(columnIndex));
+                    break;
+                case Cursor.FIELD_TYPE_NULL:
+                    contentValues.put(column, (String) null);
+                    break;
+                case Cursor.FIELD_TYPE_STRING:
+                    contentValues.put(column, cursor.getString(columnIndex));
+                    break;
+            }
+        }
+        return contentValues;
+    }
+}
---
+++ EveryThingUtils/src/main/java/chatgpt/sqlite/database/arabware/JsonToDatabase.java
@@ -0,0 +1,93 @@
+package chatgpt.sqlite.database.arabware;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonToDatabase {
+
+    public interface ProgressListener {
+        void onProgressUpdate(int progress, int max);
+        void onConversionComplete(File outputFile);
+        void onConversionError(Exception e);
+    }
+
+    private final Context mContext;
+    private final ProgressListener mListener;
+
+    public JsonToDatabase(Context context, ProgressListener listener) {
+        mContext = context;
+        mListener = listener;
+    }
+
+    public void convertJsonToDatabase(File jsonFile, File dbFile) {
+        new Thread(() -> {
+            SQLiteDatabase db = SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(), null, SQLiteDatabase.CREATE_IF_NECESSARY);
+
+            try (FileReader reader = new FileReader(jsonFile)) {
+                JsonParser parser = new JsonParser();
+                JsonArray tablesArray = parser.parse(reader).getAsJsonArray();
+                int tablesCount = tablesArray.size();
+
+                for (int i = 0; i < tablesCount; i++) {
+                    JsonObject tableObject = tablesArray.get(i).getAsJsonObject();
+                    String tableName = tableObject.get("table_name").getAsString();
+                    JsonArray tableData = tableObject.get("table_data").getAsJsonArray();
+                    int tableRowCount = tableData.size();
+
+                    db.beginTransaction();
+                    try {
+                        db.execSQL("CREATE TABLE IF NOT EXISTS " + tableName + "(_id INTEGER PRIMARY KEY AUTOINCREMENT)");
+
+                        for (int j = 0; j < tableRowCount; j++) {
+                            JsonObject rowObject = tableData.get(j).getAsJsonObject();
+                            ContentValues rowValues = new ContentValues();
+                            for (String column : rowObject.keySet()) {
+                                rowValues.put(column, getFieldValue(rowObject.get(column)));
+                            }
+                            db.insert(tableName, null, rowValues);
+                        }
+
+                        db.setTransactionSuccessful();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        mListener.onConversionError(e);
+                        return;
+                    } finally {
+                        db.endTransaction();
+                    }
+
+                    mListener.onProgressUpdate(i + 1, tablesCount);
+                }
+
+                mListener.onConversionComplete(dbFile);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                mListener.onConversionError(e);
+            } finally {
+                db.close();
+            }
+        }).start();
+    }
+
+    private Object getFieldValue(JsonElement valueElement) {
+        if (valueElement.isJsonNull()) {
+            return null;
+        } else if (valueElement.isJsonPrimitive()) {
+            return valueElement.getAsString();
+        } else {
+            return valueElement.toString();
+        }
+    }
